c4b8811fc68fa65e9c31efa37b3c7312d6558f7a
arsenicum32 <zombiehot@yandex.ru>
2017-09-17 11:56:00 +0300

    add bot

--- socket.js
+++ socket.js
@@ -1,5 +1,22 @@
 var io = require('socket.io')(1280);
 
+
+var TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+var token = '244772236:AAF8Bx9B4kTBX2uDqIymaJS9d0lgbBMRd0I';
+
+// Create a bot that uses 'polling' to fetch new updates
+var bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+  var chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Received your message');
+
+  io.sockets.in('test').emit('res', {obj: parseFloat(msg.text) } );
+});
+
+
 io.sockets.on( 'connection', function( socket ) {
   console.log('socket connect');
   socket.on('leave', function(room) {
